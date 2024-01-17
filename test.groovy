006a4610fe7e1f1a287ac0fad992725da11e6c65
Veknull <155902186+Veknull@users.noreply.github.com>
2024-01-17 09:26:58 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -7,6 +7,10 @@ pipeline {
         choice(name: 'JOUR', choices: ['1', '2', '3', '4', '5', '6', '7','8', '9', '10','11', '12','13', '14', '15', '16', '17', '18', '19','20', '21', '22','23', '24','25','26','27','28','29','30','31'], description: 'Quel est la date du jour ?')
     }
 
+    environment {
+        JOURNAL_DIR = "${env.WORKSPACE}/journals"
+    }
+
     stages {
         stage('Janvier') {
             when {
